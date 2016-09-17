bac2a11e628ca453f0c5f7e29bbf41c9dd15c26f
Korjavii <korjavii.dyu@gmail.com>
2016-09-17 22:23:32 +0300

    cssmodues added

--- webpack.config.js
+++ webpack.config.js
@@ -8,9 +8,26 @@ const root    = resolve(__dirname);
 const src     = join(root, 'src');
 const modules = join(root, 'node_modules');
 const dest    = join(root, 'dist');
+const dotenv = require('dotenv');
 
 const NODE_ENV = process.env.NODE_ENV;
+const dotEnvVars = dotenv.config();
+const environmentEnv = dotenv.config({
+  path: join(root, 'config', `${NODE_ENV}.config.js`),
+  silent: true,
+});
+const envVariables =
+    Object.assign({}, dotEnvVars, environmentEnv);
 const isDev = NODE_ENV === 'development';
+const defines =
+  Object.keys(envVariables)
+  .reduce((memo, key) => {
+    const val = JSON.stringify(envVariables[key]);
+    memo[`__${key.toUpperCase()}__`] = val;
+    return memo;
+  }, {
+    __NODE_ENV__: JSON.stringify(NODE_ENV)
+  });
 
 var config = getConfig({
   isDev: isDev,
@@ -18,5 +35,44 @@ var config = getConfig({
   out: dest,
   clearBeforeBuild: true
 });
+
+config.postcss = [].concat([
+  require('precss')({}),
+  require('autoprefixer')({}),
+  require('cssnano')({})
+]);
+
+// CSS modules
+const cssModulesNames = `${isDev ? '[path][name]__[local]__' : ''}[hash:base64:5]`;
+
+const matchCssLoaders = /(^|!)(css-loader)($|!)/;
+
+const findLoader = (loaders, match) => {
+  const found = loaders.filter(l => l && l.loader && l.loader.match(match))
+  return found ? found[0] : null;
+}
+// existing css loader
+const cssloader =
+  findLoader(config.module.loaders, matchCssLoaders);
+
+const newloader = Object.assign({}, cssloader, {
+  test: /\.module\.css$/,
+  include: [src],
+  loader: cssloader.loader.replace(matchCssLoaders, `$1$2?modules&localIdentName=${cssModulesNames}$3`)
+})
+config.module.loaders.push(newloader);
+cssloader.test = new RegExp(`[^module]${cssloader.test.source}`)
+cssloader.loader = newloader.loader
+
+config.module.loaders.push({
+  test: /\.css$/,
+  include: [modules],
+  loader: 'style!css'
+})
+// CSS modules
+
+config.plugins = [
+  new webpack.DefinePlugin(defines)
+].concat(config.plugins);
 
 module.exports = config;
