9ac074ec27fca2660773cd54883b1aa4463966b7
Korjavii <korjavii.dyu@gmail.com>
2016-09-18 11:15:39 +0300

    routes added

--- src/containers/App/App.js
+++ src/containers/App/App.js
@@ -1,18 +1,35 @@
-import React from 'react'
+import React, { PropTypes } from 'react';
+import { Router } from 'react-router';
 import ReactDOM from 'react-dom'
 
 import styles from './styles.module.css'
 
-const App = React.createClass({
-  render: function() {
+class App extends React.Component {
+  static contextTypes = {
+    router: PropTypes.object
+  }
+
+  static propTypes = {
+    history: PropTypes.object.isRequired,
+    routes: PropTypes.element.isRequired
+  };
+
+  get content() {
     return (
-      <div className={styles.wrapper}>
-        <h1>
-          <i className="fa fa-star"></i>
-          Environment: {__NODE_ENV__}</h1>
-      </div>
+      <Router
+        routes={this.props.routes}
+        history={this.props.history} />
     )
   }
-});
+
+  render () {
+     return (
+       <div style={{ height: '100%' }}>
+         {this.content}
+       </div>
+     )
+   }
+}
+
 
 module.exports = App;
